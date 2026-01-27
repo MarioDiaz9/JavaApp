643509f8379a6919e48760f71713809286a8c860
MarioDiaz9 <pindadodiazmario@gmail.com>
2026-01-27 16:51:31 +0100

    Created Pokemon List
    Removed the example code and added a new pokemon list plus a few pokemon objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-let favoriteFood = 'Risotto'
+//POKEMON TEMPLATE{ name: '', number: '', type: ['', ''] }
 
-alert('Hello world');
-
-document.write(favoriteFood)
+let pokemonList = [
+    { name: 'Bulbasaur', number: '1', type: ['grass', 'poison'] },
+    { name: 'Ivysaur', number: '2', type: ['grass', 'poison'] },
+    { name: 'Venusaur', number: '3', type: ['grass', 'poison'] },
+    { name: 'Charmander', number: '', type: 'fire' },
+    { name: 'Charmeleon', number: '', type: 'fire' },
+    { name: 'Charizard', number: '', type: ['fire', 'flying'] },
+    { name: 'Squirtle', number: '', type: 'water' },
+    { name: 'Wartortle', number: '', type: 'water' },
+    { name: 'Blastoise', number: '', type: 'water' },
+]
